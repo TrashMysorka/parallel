f1a366931fedadf911887eb4d488341f18ded4df
v.babushkin <ghostminerx@gmail.com>
2022-02-08 12:00:34 +0700

    update

--- main.cpp
+++ main.cpp
@@ -5,12 +5,11 @@ int main() {
     long long int n = 10000000;
     double sum = 0;
     double *array = (double*)malloc(n*sizeof(double));
-#pragma acc kernel
+#pragma acc kernels
     for(int i = 0; i < n; i++){
         array[i] = sin((double)i);
     }
-
-#pragma acc kernel
+#pragma acc kernels
     for (int i = 0; i < n; i++){
         sum += i;
     }
