546368137cdb6db9609c75b5c86e7fa2a3bfceea
v.babushkin <ghostminerx@gmail.com>
2022-02-08 12:16:42 +0700

    update

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
 
 #pragma acc kernels
     for(int i = 0; i < 10000000; i++){
-        array[i] = sin((double)i);
+        array[i] = sin((double)((i*2*M_PI)/10000000));
     }
 #pragma acc kernels
     for (auto i : array){
